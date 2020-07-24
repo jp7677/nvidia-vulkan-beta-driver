563dede2a6a19c634b13ff8344bf13b29bf4f5d5
Jens Peters <jp7677@gmail.com>
2020-07-24 15:07:35 +0200

    Add steps to build the actual driver.

--- make-rpms.sh
+++ make-rpms.sh
@@ -1,3 +1,13 @@
 #!/bin/sh
+mkdir -p rpmbuild/{BUILD,BUILDROOT,RPMS,SOURCES,SPECS,SRPMS}
+export VERSION=${VERSION:-450.56.02}
 
-
+pushd nvidia-driver
+wget -c https://developer.nvidia.com/vulkan-beta-${VERSION//./}-linux -O NVIDIA-Linux-x86_64-${VERSION}.run
+sh nvidia-generate-tarballs.sh
+sed -i "s/Version:.*/Version:$VERSION/" nvidia-driver.spec
+rpmbuild --define "_topdir $PWD/../rpmbuild" --define "_sourcedir $PWD" -bb nvidia-driver.spec --clean --target i386
+rpmbuild --define "_topdir $PWD/../rpmbuild" --define "_sourcedir $PWD" -bb nvidia-driver.spec --clean
+git checkout -- nvidia-driver.spec
+git clean -d -f
+popd
