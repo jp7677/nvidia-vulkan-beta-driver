b3f422bedfb46add00911ebe6ae3110f63a8f4f8
Jens Peters <jp7677@gmail.com>
2020-07-25 12:14:07 +0200

    Avoid directory switches.

--- make-rpms.sh
+++ make-rpms.sh
@@ -6,34 +6,24 @@ if [ -z "$VERSION" ]; then
     exit 1
 fi
 
-mkdir -p rpmbuild/{BUILD,BUILDROOT,RPMS,SOURCES,SPECS,SRPMS}
+mkdir -p rpmbuild/{BUILD,BUILDROOT,RPMS,SRPMS}
 [[ -f NVIDIA-Linux-x86_64-${VERSION}.run ]] || wget -c https://developer.nvidia.com/vulkan-beta-${VERSION//./}-linux -O NVIDIA-Linux-x86_64-${VERSION}.run
-sh nvidia-driver/nvidia-generate-tarballs.sh
+[[ -f nvidia-driver-${VERSION}-x86_64.tar.xz ]] || sh nvidia-driver/nvidia-generate-tarballs.sh
 
-pushd nvidia-driver
-sed -i "s/Version:.*/Version:$VERSION/" nvidia-driver.spec
-cp -v ../nvidia-driver-$VERSION-x86_64.tar.xz .
-cp -v ../nvidia-driver-$VERSION-i386.tar.xz .
-rpmbuild --define "_topdir $PWD/../rpmbuild" --define "_sourcedir $PWD" -bb nvidia-driver.spec --clean --target i386
-rpmbuild --define "_topdir $PWD/../rpmbuild" --define "_sourcedir $PWD" -bb nvidia-driver.spec --clean
-popd
+cp -v nvidia-driver-$VERSION-{x86_64,i386}.tar.xz nvidia-driver/
+cp -v nvidia-kmod-$VERSION-x86_64.tar.xz nvidia-kmod/
+cp -v nvidia-kmod-$VERSION-x86_64.tar.xz dkms-nvidia/
 
-pushd nvidia-kmod-common
-sed -i "s/Version:.*/Version:$VERSION/" nvidia-kmod-common.spec
-rpmbuild --define "_topdir $PWD/../rpmbuild" --define "_sourcedir $PWD" -bb nvidia-kmod-common.spec --clean
-popd
+sed -i "s/Version:.*/Version:$VERSION/" nvidia-driver/nvidia-driver.spec
+sed -i "s/Version:.*/Version:$VERSION/" nvidia-kmod-common/nvidia-kmod-common.spec
+sed -i "s/Version:.*/Version:$VERSION/" nvidia-kmod/nvidia-kmod.spec
+sed -i "s/Version:.*/Version:$VERSION/" dkms-nvidia/dkms-nvidia.spec
 
-pushd nvidia-kmod
-sed -i "s/Version:.*/Version:$VERSION/" nvidia-kmod.spec
-cp -v ../nvidia-kmod-$VERSION-x86_64.tar.xz .
-rpmbuild --define "_topdir $PWD/../rpmbuild" --define "_sourcedir $PWD" --define "_specdir $PWD" -bb nvidia-kmod.spec --clean
-popd
-
-pushd dkms-nvidia
-sed -i "s/Version:.*/Version:$VERSION/" dkms-nvidia.spec
-cp -v ../nvidia-kmod-$VERSION-x86_64.tar.xz .
-rpmbuild --define "_topdir $PWD/../rpmbuild" --define "_sourcedir $PWD" -bb dkms-nvidia.spec --clean
-popd
+rpmbuild --define "_topdir $PWD/rpmbuild" --define "_sourcedir $PWD/nvidia-driver" --define "_specdir $PWD/nvidia-driver" -bb nvidia-driver/nvidia-driver.spec --clean --target i386
+rpmbuild --define "_topdir $PWD/rpmbuild" --define "_sourcedir $PWD/nvidia-driver" --define "_specdir $PWD/nvidia-driver" -bb nvidia-driver/nvidia-driver.spec --clean
+rpmbuild --define "_topdir $PWD/rpmbuild" --define "_sourcedir $PWD/nvidia-kmod-common" --define "_specdir $PWD/nvidia-kmod-common" -bb nvidia-kmod-common/nvidia-kmod-common.spec --clean
+rpmbuild --define "_topdir $PWD/rpmbuild" --define "_sourcedir $PWD/nvidia-kmod" --define "_specdir $PWD/nvidia-kmod" -bb nvidia-kmod/nvidia-kmod.spec --clean
+rpmbuild --define "_topdir $PWD/rpmbuild" --define "_sourcedir $PWD/dkms-nvidia" --define "_specdir $PWD/dkms-nvidia" -bb dkms-nvidia/dkms-nvidia.spec --clean
 
 git submodule foreach --recursive git clean -d -f
 git submodule foreach --recursive git reset --hard
