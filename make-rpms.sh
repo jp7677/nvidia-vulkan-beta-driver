65cce25b5fbf89d9159404b0e17a8b61f6743c21
Jens Peters <jp7677@gmail.com>
2020-07-25 12:56:21 +0200

    Simplify sed and revert actions. Formatting.

--- make-rpms.sh
+++ make-rpms.sh
@@ -13,14 +13,14 @@ ln -s $PWD/nvidia-driver-$VERSION-{x86_64,i386}.tar.xz nvidia-driver/
 ln -s $PWD/nvidia-kmod-$VERSION-x86_64.tar.xz nvidia-kmod/
 ln -s $PWD/nvidia-kmod-$VERSION-x86_64.tar.xz dkms-nvidia/
 
-sed -i "s/Version:.*/Version:$VERSION/" nvidia-driver/nvidia-driver.spec nvidia-kmod-common/nvidia-kmod-common.spec nvidia-kmod/nvidia-kmod.spec dkms-nvidia/dkms-nvidia.spec
+git submodule foreach 'sed -i "s/Version:.*/Version:$VERSION/" $name.spec'
 
 mkdir -p rpmbuild/{BUILD,BUILDROOT,RPMS,SRPMS}
-rpmbuild --define "_topdir $PWD/rpmbuild" --define "_sourcedir $PWD/nvidia-driver" --define "_specdir $PWD/nvidia-driver" -bb nvidia-driver/nvidia-driver.spec --clean --target i386
-rpmbuild --define "_topdir $PWD/rpmbuild" --define "_sourcedir $PWD/nvidia-driver" --define "_specdir $PWD/nvidia-driver" -bb nvidia-driver/nvidia-driver.spec --clean
-rpmbuild --define "_topdir $PWD/rpmbuild" --define "_sourcedir $PWD/nvidia-kmod-common" --define "_specdir $PWD/nvidia-kmod-common" -bb nvidia-kmod-common/nvidia-kmod-common.spec --clean
-rpmbuild --define "_topdir $PWD/rpmbuild" --define "_sourcedir $PWD/nvidia-kmod" --define "_specdir $PWD/nvidia-kmod" -bb nvidia-kmod/nvidia-kmod.spec --clean
-rpmbuild --define "_topdir $PWD/rpmbuild" --define "_sourcedir $PWD/dkms-nvidia" --define "_specdir $PWD/dkms-nvidia" -bb dkms-nvidia/dkms-nvidia.spec --clean
+rpmbuild --define "_topdir $PWD/rpmbuild" --define "_sourcedir $PWD/nvidia-driver" --define "_specdir $PWD/nvidia-driver" --clean --target i386 -bb nvidia-driver/nvidia-driver.spec
+rpmbuild --define "_topdir $PWD/rpmbuild" --define "_sourcedir $PWD/nvidia-driver" --define "_specdir $PWD/nvidia-driver" --clean -bb nvidia-driver/nvidia-driver.spec
+rpmbuild --define "_topdir $PWD/rpmbuild" --define "_sourcedir $PWD/nvidia-kmod-common" --define "_specdir $PWD/nvidia-kmod-common" --clean -bb nvidia-kmod-common/nvidia-kmod-common.spec
+rpmbuild --define "_topdir $PWD/rpmbuild" --define "_sourcedir $PWD/nvidia-kmod" --define "_specdir $PWD/nvidia-kmod" --clean -bb nvidia-kmod/nvidia-kmod.spec
+rpmbuild --define "_topdir $PWD/rpmbuild" --define "_sourcedir $PWD/dkms-nvidia" --define "_specdir $PWD/dkms-nvidia" --clean -bb dkms-nvidia/dkms-nvidia.spec
 
+git submodule foreach 'git checkout -- $name.spec'
 git submodule foreach git clean -d -f
-git submodule foreach git checkout -- *.spec
