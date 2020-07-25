c2d9d109e1d255f55e16049d77a7ca56e52eee7c
Jens Peters <jp7677@gmail.com>
2020-07-25 13:55:51 +0200

    Create links while looping over git submodules.

--- make-rpms.sh
+++ make-rpms.sh
@@ -9,11 +9,9 @@ fi
 [[ -f NVIDIA-Linux-x86_64-${VERSION}.run ]] || wget -c https://developer.nvidia.com/vulkan-beta-${VERSION//./}-linux -O NVIDIA-Linux-x86_64-${VERSION}.run
 [[ -f nvidia-driver-${VERSION}-x86_64.tar.xz ]] || sh nvidia-driver/nvidia-generate-tarballs.sh
 
-ln -s $PWD/nvidia-driver-$VERSION-{x86_64,i386}.tar.xz nvidia-driver/
-ln -s $PWD/nvidia-kmod-$VERSION-x86_64.tar.xz nvidia-kmod/
-ln -s $PWD/nvidia-kmod-$VERSION-x86_64.tar.xz dkms-nvidia/
-
-git submodule foreach 'sed -i "s/Version:.*/Version:$VERSION/" $name.spec'
+git submodule foreach '\
+    sed -i "s/Version:.*/Version:$VERSION/" $name.spec && \
+    ln -s ../nvidia-driver-$VERSION-{x86_64,i386}.tar.xz ../nvidia-kmod-$VERSION-x86_64.tar.xz .'
 
 mkdir -p rpmbuild/{BUILD,BUILDROOT,RPMS,SRPMS}
 rpmbuild --define "_topdir $PWD/rpmbuild" --define "_sourcedir $PWD/nvidia-driver" --define "_specdir $PWD/nvidia-driver" --clean --target i386 -bb nvidia-driver/nvidia-driver.spec
@@ -22,4 +20,6 @@ rpmbuild --define "_topdir $PWD/rpmbuild" --define "_sourcedir $PWD/nvidia-kmod-
 rpmbuild --define "_topdir $PWD/rpmbuild" --define "_sourcedir $PWD/nvidia-kmod" --define "_specdir $PWD/nvidia-kmod" --clean -bb nvidia-kmod/nvidia-kmod.spec
 rpmbuild --define "_topdir $PWD/rpmbuild" --define "_sourcedir $PWD/dkms-nvidia" --define "_specdir $PWD/dkms-nvidia" --clean -bb dkms-nvidia/dkms-nvidia.spec
 
-git submodule foreach 'git checkout -- $name.spec && rm -f nvidia-{driver,kmod}-$VERSION-{x86_64,i386}.tar.xz'
+git submodule foreach '\
+    git checkout -- $name.spec && \
+    rm nvidia-driver-$VERSION-{x86_64,i386}.tar.xz nvidia-kmod-$VERSION-x86_64.tar.xz'
