f0222f8bd98a20462f2f604a13b19d18135e19cd
Jens Peters <jp7677@gmail.com>
2020-07-30 07:19:55 +0200

    Introduce main function.

--- make-rpms.sh
+++ make-rpms.sh
@@ -1,22 +1,33 @@
 #!/bin/sh
 set -e
 
-if [ -z ${VERSION} ]; then
-    echo "Please specify VERSION to use this script."
-    exit 1
-fi
+main() {
+    if [ -z ${VERSION} ]; then
+        echo "Please specify VERSION to use this script."
+        exit 1
+    fi
 
-[[ -f NVIDIA-Linux-x86_64-${VERSION}.run ]] || \
-    wget -c https://developer.nvidia.com/vulkan-beta-${VERSION//./}-linux -O NVIDIA-Linux-x86_64-${VERSION}.run
+    [[ -f NVIDIA-Linux-x86_64-${VERSION}.run ]] || \
+        wget -c https://developer.nvidia.com/vulkan-beta-${VERSION//./}-linux -O NVIDIA-Linux-x86_64-${VERSION}.run
 
-[[ -f nvidia-driver-${VERSION}-x86_64.tar.xz ]] || \
-    sh nvidia-driver/nvidia-generate-tarballs.sh
+    [[ -f nvidia-driver-${VERSION}-x86_64.tar.xz ]] || \
+        sh nvidia-driver/nvidia-generate-tarballs.sh
 
-git submodule foreach '\
-    sed -i "s/Version:.*/Version:$VERSION/" $name.spec && \
-    ln -s ../nvidia-driver-$VERSION-{x86_64,i386}.tar.xz ../nvidia-kmod-$VERSION-x86_64.tar.xz .'
+    git submodule foreach '\
+        sed -i "s/Version:.*/Version:$VERSION/" $name.spec && \
+        ln -s ../nvidia-driver-$VERSION-{x86_64,i386}.tar.xz ../nvidia-kmod-$VERSION-x86_64.tar.xz .'
 
-mkdir -p rpmbuild/{BUILD,BUILDROOT,RPMS,SRPMS}
+    mkdir -p rpmbuild/{BUILD,BUILDROOT,RPMS,SRPMS}
+    build_rpm nvidia-driver i386
+    build_rpm nvidia-driver x86_64
+    build_rpm nvidia-kmod-common x86_64
+    build_rpm nvidia-kmod x86_64
+    build_rpm dkms-nvidia x86_64
+
+    git submodule foreach '\
+        git checkout -- $name.spec && \
+        rm nvidia-driver-$VERSION-{x86_64,i386}.tar.xz nvidia-kmod-$VERSION-x86_64.tar.xz'
+}
 
 build_rpm() {
     rpmbuild \
@@ -28,12 +39,4 @@ build_rpm() {
         -bb ${1}/${1}.spec    
 }
 
-build_rpm nvidia-driver i386
-build_rpm nvidia-driver x86_64
-build_rpm nvidia-kmod-common x86_64
-build_rpm nvidia-kmod x86_64
-build_rpm dkms-nvidia x86_64
-
-git submodule foreach '\
-    git checkout -- $name.spec && \
-    rm nvidia-driver-$VERSION-{x86_64,i386}.tar.xz nvidia-kmod-$VERSION-x86_64.tar.xz'
+main ${@}
