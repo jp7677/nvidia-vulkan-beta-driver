827e6a7647c194616e6177be79787796bc5ab3d7
Jens Peters <jp7677@gmail.com>
2020-07-30 07:11:29 +0200

    Use function.

--- make-rpms.sh
+++ make-rpms.sh
@@ -17,41 +17,22 @@ git submodule foreach '\
     ln -s ../nvidia-driver-$VERSION-{x86_64,i386}.tar.xz ../nvidia-kmod-$VERSION-x86_64.tar.xz .'
 
 mkdir -p rpmbuild/{BUILD,BUILDROOT,RPMS,SRPMS}
-rpmbuild \
-    --define "_topdir $PWD/rpmbuild" \
-    --define "_sourcedir $PWD/nvidia-driver" \
-    --define "_specdir $PWD/nvidia-driver" \
-    --target i386 \
-    --clean \
-    -bb nvidia-driver/nvidia-driver.spec
-rpmbuild \
-    --define "_topdir $PWD/rpmbuild" \
-    --define "_sourcedir $PWD/nvidia-driver" \
-    --define "_specdir $PWD/nvidia-driver" \
-    --target x86_64 \
-    --clean \
-    -bb nvidia-driver/nvidia-driver.spec
-rpmbuild \
-    --define "_topdir $PWD/rpmbuild" \
-    --define "_sourcedir $PWD/nvidia-kmod-common" \
-    --define "_specdir $PWD/nvidia-kmod-common" \
-    --target x86_64 \
-    --clean \
-    -bb nvidia-kmod-common/nvidia-kmod-common.spec
-rpmbuild \
-    --define "_topdir $PWD/rpmbuild" \
-    --define "_sourcedir $PWD/nvidia-kmod" \
-    --define "_specdir $PWD/nvidia-kmod" \
-    --target x86_64 \
-    --clean \
-    -bb nvidia-kmod/nvidia-kmod.spec
-rpmbuild \
-    --define "_topdir $PWD/rpmbuild" \
-    --define "_sourcedir $PWD/dkms-nvidia" \
-    --define "_specdir $PWD/dkms-nvidia" \
-    --target x86_64 \
-    --clean \
-    -bb dkms-nvidia/dkms-nvidia.spec
+
+build_rpm() {
+    rpmbuild \
+        --define "_topdir $PWD/rpmbuild" \
+        --define "_sourcedir $PWD/$1" \
+        --define "_specdir $PWD/$1" \
+        --target $2 \
+        --clean \
+        -bb $1/$1.spec    
+}
+
+build_rpm nvidia-driver i386
+build_rpm nvidia-driver x86_64
+build_rpm nvidia-kmod-common x86_64
+build_rpm nvidia-kmod x86_64
+build_rpm dkms-nvidia x86_64
 
 git submodule foreach '\
     git checkout -- $name.spec && \
